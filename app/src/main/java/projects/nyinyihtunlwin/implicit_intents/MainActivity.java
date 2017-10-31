4266a89bbc87e7a373f25c1a8d88fc96dfc46562
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2017-10-31 15:28:37 +0630

    ui updated

--- app/src/main/java/projects/nyinyihtunlwin/implicit_intents/MainActivity.java
+++ app/src/main/java/projects/nyinyihtunlwin/implicit_intents/MainActivity.java
@@ -1,13 +1,44 @@
 package projects.nyinyihtunlwin.implicit_intents;
 
+import android.content.Intent;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+        btnAlarm.setOnClickListener(this);
+    }
+
+    private void initializeViews() {
+        btnAlarm = (Button) findViewById(R.id.btn_alarm);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_alarm:
+                createAlarm("Hello Nyi Nyi", 12, 11);
+                break;
+        }
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
